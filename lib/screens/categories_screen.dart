a0aae863291a1364affd5af8639fc7f18bdd1cec
Ryss-D <70545185+Ryss-D@users.noreply.github.com>
2022-06-28 09:25:19 -0500

    feat: added basic app grid structure

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../category_item.dart';
+
+import '../dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeals'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES
+            .map(
+              (e) => CategoryItem(title: e.title, color: e.color),
+            )
+            .toList(),
+        //Slivers is just a scrollable area on screen
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}

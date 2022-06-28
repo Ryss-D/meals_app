feac8e0d69723a79bcd710fb33939d7541769fdd
Ryss-D <70545185+Ryss-D@users.noreply.github.com>
2022-06-28 17:36:11 -0500

    feat: added bottom tab bar to improve lookin

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -8,32 +8,63 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Widget> _pages = [
+    CategoriesScreen(),
+    FavoritesScreen(),
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Meals'),
-            bottom: const TabBar(
-              tabs: <Widget>[
-                Tab(
-                  icon: Icon(Icons.category),
-                  text: 'Categories',
-                ),
-                Tab(
-                  icon: Icon(Icons.star),
-                  text: 'Favorites',
-                ),
-              ],
+    // return DefaultTabController(
+    // initialIndex: 0,
+    // length: 2,
+    // child: Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meals'),
+        bottom: const TabBar(
+          tabs: <Widget>[
+            Tab(
+              icon: Icon(Icons.category),
+              text: 'Categories',
             ),
+            Tab(
+              icon: Icon(Icons.star),
+              text: 'Favorites',
+            ),
+          ],
+        ),
+      ),
+      // body: TabBarView(
+      //   children: [
+      //     CategoriesScreen(),
+      //     FavoritesScreen(),
+      // ],
+      // ),
+      body: _pages[_selectedPageIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Categories',
           ),
-          body: TabBarView(
-            children: [
-              CategoriesScreen(),
-              FavoritesScreen(),
-            ],
-          )),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+          )
+        ],
+      ),
     );
+    // );
   }
 }
